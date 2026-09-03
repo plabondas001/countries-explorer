0ebbd884f95a3e2393d9e9670819c1edd51a7b9e
Plabondas001 <plabondas001@gmail.com>
2026-09-03 12:16:09 +0600

    added visited button and apply  conditional style

--- src/components/Country/Country.tsx
+++ src/components/Country/Country.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { CountriesType } from "../../type";
 import "./Country.css";
 
@@ -6,13 +7,22 @@ export interface CountryProps {
 }
 
 export default function Country({ country }: CountryProps) {
+  const [visit, setVisit] = useState<boolean>(false);
+
+  const handleVisit = () => {
+    setVisit(!visit);
+  };
+
   return (
-    <div className="country">
+    <div className={`country ${visit ? "country-visited" : ""}`}>
       <h1>{country.name.common}</h1>
       <img src={country.flags.flags.png} alt="" />
       <h3>Capital: {country.capital.capital}</h3>
       <h4>Region: {country.region.region}</h4>
       <p>population: {country.population.population}</p>
+      <button onClick={handleVisit} className="btn">
+        {visit ? "Visited" : "Mark as Visited"}
+      </button>
     </div>
   );
 }
